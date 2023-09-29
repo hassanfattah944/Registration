da300ed25531c90a2f4ca386d1a1cd6b93673245
H.F <125692421+hassanfattah944@users.noreply.github.com>
2023-09-29 10:26:30 +0300

    Update d.py

--- R1/d.py
+++ R1/d.py
@@ -1,13 +1,20 @@
-# streamlit_app.py
 import streamlit as st
+
 # Initialize connection.
 conn = st.experimental_connection("postgresql", type="sql")
-# Perform query.
+
+# Perform query to retrieve user data.
 df = conn.query('SELECT * FROM users;')
-# Print results.
+
+# Create a Streamlit table to display user data and add a delete button.
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.email}:")
-    # Display the results in a table with custom formatting.
-# Display the results in a table using Markdown.
-# Print the results
+    st.write(f"{row.name} has an email: {row.email}")
+    
+    # Add a delete button for each user
+    if st.button(f"Delete {row.name}"):
+        # Use the user's unique identifier (e.g., user_id) to delete the record
+        conn.query(f'DELETE FROM users WHERE user_id = {row.user_id};')
+        st.success(f"{row.name}'s record has been deleted.")
+
+# Display the updated results in a table using Markdown.
 st.write(df)
