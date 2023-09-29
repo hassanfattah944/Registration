55643924095f6331e072d3e0a2cf8306d1e3c7b1
H.F <125692421+hassanfattah944@users.noreply.github.com>
2023-09-29 10:25:02 +0300

    Update registration.py

--- R1/pages/registration.py
+++ R1/pages/registration.py
@@ -17,17 +17,25 @@ email = form.text_input("Email")
 password = form.text_input("Password")
 submit = form.form_submit_button("Submit")
 
-# If the user submits the form, save the data to the database
+# If the user submits the form, validate the data and save it to the database
 if submit:
-    # Insert the user's registration information into the database
-    cur.execute(
-        "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
-        (name, email, password),
-    )
-    conn.commit()
+    # Check if any of the fields are empty
+    if not name or not email or not password:
+        st.error("Please fill in all fields.")
+    else:
+        try:
+            # Insert the user's registration information into the database using parameterized query
+            cur.execute(
+                "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
+                (name, email, password),
+            )
+            conn.commit()
 
-    # Display a success message to the user
-    st.success("Your registration was successful!")
+            # Display a success message to the user
+            st.success("Your registration was successful!")
+
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
 
 # Close the database connection
 cur.close()
